232bccdaadb2100e262fb7c0bccce1a7332c55af
nemi21 <48768261+nemi21@users.noreply.github.com>
2025-02-24 19:31:32 -0500

    Finish Player class

--- src/ttt/Player.java
+++ src/ttt/Player.java
@@ -2,14 +2,32 @@ package ttt;
 
 public class Player {
 	//Stores player info(symbol, score).
-	private int scoreX;
-	private int scoreO;
-	private char player;
+	private int score;
+	private char symbol;
 	
-	Player(int scoreX, int scoreO, char player) {
-		this.scoreX = scoreX;
-		this.scoreO = scoreO;
-		this.player = player;
-	}//make constructor for player
+	public Player(int score, char symbol) {
+		this.score = 0;// The Default score
+		this.symbol = symbol;
+	}
+
+	//Method to get the score
+	public int getScore() {
+		return score;
+	}
+	
+	//Method to count the score instead of doing it manually
+	public void incrementScore() {
+		this.score++;
+	}
+
+	//Method to get the player symbol 
+	public char getSymbol() {
+		return symbol;
+	}
+
+	@Override
+	public String toString() {
+		return "Player " + symbol + " | Score: " + score;
+	}
 	
 }
