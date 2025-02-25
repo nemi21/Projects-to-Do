ff55302f40232333cebcec40da92b3a080859a57
nemi21 <48768261+nemi21@users.noreply.github.com>
2025-02-24 19:45:09 -0500

    Starting wokring on the main and create the player objects

--- src/ttt/TicTacToeApp.java
+++ src/ttt/TicTacToeApp.java
@@ -5,7 +5,17 @@ public class TicTacToeApp {
 	public static void main(String[] args) {
 		// Controls the game flow(creating players, starting the game)
 		
-
+		
+		//1.Initialize the game (create players, reset board)
+		//Create player for the TTT game and set their for each at zero
+		Player playerOne = new Player(0, 'X');
+		Player playerTwo = new Player(0, 'O');
+		//2.Game loop (keep playing until win or draw)
+		//3.Switch player turns
+		//4.Take player input and validate it
+		//5.Check for win or draw
+		//6.Update and display scores
+		 
 	}
 
 }
